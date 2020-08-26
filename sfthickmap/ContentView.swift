882aa3937b4172a2225f8d21d43d36c912cbae24
Andrew W. Chen <andrew.w.chen.24@dartmouth.edu>
2020-08-26 01:02:19 -0400

    Initial Commit

---
+++ sfthickmap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sfthickmap
+//
+//  Created by Andrew Chen on 8/26/20.
+//  Copyright © 2020 Dartmouth DEV Studio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
