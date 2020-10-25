f570c45d8e2d0b4f84e133dbb318d8ae9b897993
Andrew W. Chen <andrew.w.chen.24@dartmouth.edu>
2020-10-25 01:08:11 -0400

    add AnnotationDetailView.swift

--- sfthickmap/AnnotationDetailView.swift
+++ sfthickmap/AnnotationDetailView.swift
@@ -8,11 +8,12 @@
 
 import Foundation
 import SwiftUI
+import UIKit
 import PartialSheet
 import AVKit
+import AVFoundation
 
-
-class PlayerUIView: UIView {
+class PlayerLayerUIView: UIView {
     private let playerLayer = AVPlayerLayer()
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -34,27 +35,54 @@ class PlayerUIView: UIView {
 
 }
 
-struct PlayerView: UIViewRepresentable {
-    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
+struct PlayerLayerView: UIViewRepresentable {
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerLayerView>) {
         
     }
     func makeUIView(context: Context) -> UIView {
-        return PlayerUIView(frame: .zero)
+        return PlayerLayerUIView(frame: .zero)
         
     }
     
 }
 
+
+struct AVPlayerView: UIViewControllerRepresentable {
+
+    @Binding var videoURL: URL?
+    
+    private var player: AVPlayer {
+        return AVPlayer(url: videoURL!)
+    }
+
+    func updateUIViewController(_ playerController: AVPlayerViewController, context: Context) {
+        playerController.modalPresentationStyle = .fullScreen
+        playerController.player = player
+        playerController.player?.play()
+    }
+
+    func makeUIViewController(context: Context) -> AVPlayerViewController {
+        return AVPlayerViewController()
+    }
+}
+
+
 struct AnnotationDetailView: View {
     @EnvironmentObject var selections: Selections
     @EnvironmentObject var partialSheetManager: PartialSheetManager
+    let toPresent = UIHostingController(rootView: AnyView(EmptyView()))
+    @State private var vURL = URL(string: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8")
+
     var body: some View {
         let annoTitle = "no title"//selections.currentAnnotation!.title!
         let annoDesc = "no description"//selections.currentAnnotation.desc
+        
+
+            
         return VStack {
-                Text(annoTitle)
-                PlayerView()
-                Text(annoDesc)
+            Text(annoTitle)
+            AVPlayerView(videoURL: self.$vURL).transition(.move(edge: .bottom)).edgesIgnoringSafeArea(.all)
+            Text(annoDesc)
                 Button(action: {
                     }) {
                         HStack(alignment: .center) {
